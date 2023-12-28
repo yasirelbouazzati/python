b87bb4e43109f749d060f3dbedac2099a4d61a87
yasirelbouazzati <yasir12bouazzati@gmail.com>
2023-12-28 13:07:05 +0100

    exercice in progress

--- 1.py
+++ 1.py
@@ -1,3 +1,97 @@
-hola = [1,2,3,4,5]
 
-print(hola[3])
+#Types of Varibles
+
+
+#Boolean variable
+#boolean_1=True
+#booelan_2 =False
+#print(boolean_1 is not False)
+
+#Const variable
+#num1=10
+#num2=10
+#print(num1+num2)
+
+#String variables
+#name= "Yasir el Bouazzati el Ghalbzouri "
+#boss= "hello "
+#print(boss+name+"!!!")
+
+
+# Variables y Operadores
+
+# Ejercicio 1.1
+# Crea dos variables, a y b, y asígnales valores numéricos.
+# Luego, imprime la suma, resta, multiplicación y división de estas dos variables.
+
+# a = 10
+# b = 10
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+
+
+
+# Ejercicio 1.2
+# Crea una variable nombre y asigna tu nombre a ella.
+# Imprime un mensaje de saludo que incluya tu nombre.
+
+# Estructuras de Control
+
+# nombre = "Yasir"
+# print("hola "  + nombre )
+# print("Adios "  + nombre )
+
+# Ejercicio 2.1
+# Escribe un programa que solicite al usuario ingresar un número.
+# Luego, determina si el número es par o impar e imprime el resultado.
+
+
+#chat= int(input("ingresar numero :"))
+
+#if chat % 2 == 0:
+    #print("El numero " + str(chat) + " es numero par")
+#else:            
+    #print("El numero "+ str(chat) + " es un numero impar")
+
+# Ejercicio 2.2
+# Crea un programa que pregunte al usuario su edad.
+# Luego, determina si es mayor de edad (18 años o más) e imprime un mensaje correspondiente.
+
+# Listas y Ciclos
+    
+#print("Que edad tienes?")
+
+#chat1= int(input("ingrese su edad :"))  
+
+#if 1 <= chat1 <= 18:
+    #print("Eres menor de edad")
+#elif chat1 >= 18:
+     #   print("Eres mañor de edad")
+#else:
+ #     print("Porfavor ingrese una edad valida :) ")
+
+# Ejercicio 3.1
+# Crea una lista de números y utiliza un bucle para imprimir cada número de la lista.
+
+numeros=["1","2","3","4","5"]
+
+for numero in numeros:
+    print(numero)
+
+
+
+# Ejercicio 3.2
+# Escribe un programa que sume todos los números pares del 1 al 10 e imprima el resultado.
+
+# Funciones
+
+# Ejercicio 4.1
+# Define una función llamada cuadrado que tome un número como argumento y devuelva su cuadrado.
+# Luego, utiliza la función con un número de tu elección.
+
+# Ejercicio 4.2
+# Crea una función llamada saludo que tome el nombre de una persona como argumento
+# e imprima un mensaje de saludo personalizado.
