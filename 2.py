2892f1e551071b32f3ceb0877ed334bf182023b4
yasirelbouazzati <yasir12bouazzati@gmail.com>
2024-01-05 11:52:26 +0100

    teory of python

---
+++ 2.py
@@ -0,0 +1,82 @@
+# Actividad 1: Saludo Personalizado
+# Escribe un programa que solicite al usuario su nombre y luego imprima un mensaje de saludo personalizado.
+
+#nombre= input("what is your name? ")
+
+#print(f"hola {nombre}")
+
+
+# Actividad 2: Calculadora Simple
+# Crea una calculadora simple que pueda realizar operaciones básicas como suma, resta, multiplicación y división.
+# Pide al usuario que ingrese dos números y la operación que desea realizar.
+
+#numero1 = int (input("ingrese tu numero: "))
+#operador = str(input("elija un operador = +,-,*,/ :"))
+#numero2 = int (input("ingrese tu numero: "))
+#if operador == "+":
+ #print(numero1 + numero2)
+#if operador == "-":
+ #print(numero1 - numero2)
+ #if operador == "*":
+  #print(numero1 * numero2)
+  #if operador == "/":
+   #print(numero1 / numero2)
+   #if operador == "%":
+    #print(numero1 % numero2)
+
+# Actividad 3: Lista de Números Pares
+# Escribe un programa que genere una lista de números pares del 1 al 20 e imprímela.
+
+#for numeros in range(1,20):
+ #if numeros % 2 == 0:
+  #print(f"{numeros} es numero par" )
+ #else :
+  #print(f"{numeros} es impar")   
+
+# Actividad 4: Conversor de Temperatura
+# Crea un programa que convierta la temperatura de grados Celsius a Fahrenheit.
+# Pide al usuario que ingrese la temperatura en Celsius y muestra el resultado en Fahrenheit.
+
+
+
+
+
+
+
+
+# Actividad 5: Adivina el Número
+# Genera un número aleatorio entre 1 y 10 y pide al usuario que adivine el número.
+# Proporciona pistas si el número ingresado es demasiado alto o bajo.
+
+#chat = int(input("adivibna el numero : "))
+
+ #import random
+#numero = random.randint(1,10)
+
+#if chat == numero:
+ #  print(f"adivinaste el numero {numero} :) ")
+#else:
+   #print(f"no adivinaste el numero {numero} :(")
+
+
+# Actividad 6: Contador de Vocales
+# Escribe un programa que cuente el número de vocales en una palabra ingresada por el usuario.
+
+
+
+
+
+
+
+# Actividad 7: Tabla de Multiplicar
+# Crea un programa que imprima la tabla de multiplicar de un número ingresado por el usuario.
+
+# Actividad 8: Ordenar una Lista
+# Pide al usuario que ingrese una lista de números y luego ordena la lista de manera ascendente.
+# Imprime la lista ordenada.
+
+# Actividad 9: Verificador de Palíndromos
+# Escribe un programa que verifique si una palabra ingresada por el usuario es un palíndromo (se lee igual de izquierda a derecha que de derecha a izquierda).
+
+# Actividad 10: Contador de Palabras
+# Crea un programa que cuente el número de palabras en una oración ingresada por el usuario.
